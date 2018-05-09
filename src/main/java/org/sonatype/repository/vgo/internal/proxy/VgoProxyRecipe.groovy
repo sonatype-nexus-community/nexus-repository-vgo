e42eb80ebd0e4e69761c3cdb33d766631654a27c
DarthHater <jeffryxtron@gmail.com>
2018-05-09 07:49:00 -0800

    ProxyRecipe basics

---
+++ src/main/java/org/sonatype/repository/vgo/internal/proxy/VgoProxyRecipe.groovy
@@ -0,0 +1,153 @@
+/*
+ * Sonatype Nexus (TM) Open Source Version
+ * Copyright (c) 2018-present Sonatype, Inc.
+ * All rights reserved. Includes the third-party code listed at http://links.sonatype.com/products/nexus/oss/attributions.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License Version 1.0,
+ * which accompanies this distribution and is available at http://www.eclipse.org/legal/epl-v10.html.
+ *
+ * Sonatype Nexus (TM) Professional Version is available from Sonatype, Inc. "Sonatype" and "Sonatype Nexus" are trademarks
+ * of Sonatype, Inc. Apache Maven is a trademark of the Apache Software Foundation. M2eclipse is a trademark of the
+ * Eclipse Foundation. All other trademarks are the property of their respective owners.
+ */
+package org.sonatype.repository.vgo.internal.proxy
+
+import javax.annotation.Nonnull
+import javax.inject.Inject
+import javax.inject.Named
+import javax.inject.Provider
+import javax.inject.Singleton
+
+import org.sonatype.nexus.repository.Format
+import org.sonatype.nexus.repository.Repository
+import org.sonatype.nexus.repository.Type
+import org.sonatype.nexus.repository.http.HttpHandlers
+import org.sonatype.nexus.repository.proxy.ProxyHandler
+import org.sonatype.nexus.repository.types.ProxyType
+import org.sonatype.nexus.repository.view.ConfigurableViewFacet
+import org.sonatype.nexus.repository.view.Context
+import org.sonatype.nexus.repository.view.Matcher
+import org.sonatype.nexus.repository.view.Route
+import org.sonatype.nexus.repository.view.Router
+import org.sonatype.nexus.repository.view.ViewFacet
+import org.sonatype.nexus.repository.view.handlers.BrowseUnsupportedHandler
+import org.sonatype.nexus.repository.view.matchers.ActionMatcher
+import org.sonatype.nexus.repository.view.matchers.logic.LogicMatchers
+import org.sonatype.nexus.repository.view.matchers.token.TokenMatcher
+import org.sonatype.repository.vgo.AssetKind
+import org.sonatype.repository.vgo.VgoFormat
+import org.sonatype.repository.vgo.internal.VgoRecipeSupport
+
+import static org.sonatype.nexus.repository.http.HttpMethods.GET
+import static org.sonatype.nexus.repository.http.HttpMethods.HEAD
+import static org.sonatype.repository.vgo.AssetKind.*
+
+/**
+ * Vgo proxy repository recipe.
+ *
+ * @since 0.0.1
+ */
+@Named(VgoProxyRecipe.NAME)
+@Singleton
+class VgoProxyRecipe
+  extends VgoRecipeSupport
+{
+  private static final String NAME = 'vgo-proxy'
+
+  @Inject
+  Provider<VgoProxyFacetImpl> proxyFacet
+
+  @Inject
+  ProxyHandler proxyHandler
+
+  @Inject
+  VgoProxyRecipe(@Named(ProxyType.NAME) final Type type,
+                  @Named(VgoFormat.NAME) final Format format) {
+    super(type, format)
+  }
+
+  @Override
+  void apply(@Nonnull final Repository repository) throws Exception {
+    repository.attach(securityFacet.get())
+    repository.attach(configure(viewFacet.get()))
+    repository.attach(httpClientFacet.get())
+    repository.attach(negativeCacheFacet.get())
+    repository.attach(componentMaintenanceFacet.get())
+    repository.attach(proxyFacet.get())
+    repository.attach(storageFacet.get())
+    repository.attach(searchFacet.get())
+    repository.attach(purgeUnusedFacet.get())
+    repository.attach(attributesFacet.get())
+  }
+
+  /**
+   * Matcher for .info files
+   */
+  static Matcher infoMatcher() {
+    createMatcher(VGO_INFO, '.info')
+  }
+
+  /**
+   * Matcher for .mod files
+   */
+  static Matcher moduleMatcher() {
+    createMatcher(VGO_MODULE, '.mod')
+  }
+
+  /**
+   * Matcher for .zip files
+   */
+  static Matcher packageMatcher() {
+    createMatcher(VGO_PACKAGE, '.zip')
+  }
+
+  static Matcher createMatcher(final AssetKind assetKind, final String extension) {
+    LogicMatchers.and(
+        new ActionMatcher(GET, HEAD),
+        tokenMatcherForExtension(extension),
+        new Matcher() {
+          @Override
+          boolean matches(final Context context) {
+            context.attributes.set(AssetKind.class, assetKind)
+            return true
+          }
+        }
+    )
+  }
+
+  static TokenMatcher tokenMatcherForExtension(final String extension) {
+    new TokenMatcher("{path:.+}/{module:.+}/@v/{version:.+}.{extension:${extension}}")
+  }
+
+  /**
+   * Configure {@link ViewFacet}.
+   */
+  private ViewFacet configure(final ConfigurableViewFacet facet) {
+    Router.Builder builder = new Router.Builder()
+
+    [infoMatcher(), packageMatcher(), moduleMatcher()].each { matcher ->
+      builder.route(new Route.Builder().matcher(matcher)
+          .handler(timingHandler)
+          .handler(securityHandler)
+          .handler(exceptionHandler)
+          .handler(handlerContributor)
+          .handler(negativeCacheHandler)
+          .handler(partialFetchHandler)
+          .handler(contentHeadersHandler)
+          .handler(unitOfWorkHandler)
+          .handler(proxyHandler)
+          .create())
+    }
+
+    builder.route(new Route.Builder()
+        .matcher(BrowseUnsupportedHandler.MATCHER)
+        .handler(browseUnsupportedHandler)
+        .create())
+
+    builder.defaultHandlers(HttpHandlers.notFound())
+
+    facet.configure(builder.create())
+
+    return facet
+  }
+}
